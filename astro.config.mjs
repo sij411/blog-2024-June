d2228aa2d673ad807fb9a0371c65fdc8fd281a76
Jiwon Kwon <kjiwon411@gmail.com>
2024-06-13 12:11:33 +0900

    Config the site option

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sij411.github.io',
+	base: '/blog-2024-june',
 	integrations: [mdx(), sitemap()],
 });
