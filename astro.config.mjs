296c3745725669b948ed609fc56652a0bf7ab447
Jiwon Kwon <kjiwon411@gmail.com>
2024-06-16 18:16:38 +0900

    Update config: site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sij411.github.io',
+	site: 'https://sij411.github.io/blog-2024-June',
 	integrations: [mdx(), sitemap()],
-	base: '/blog-2024-June',
+	
 });
